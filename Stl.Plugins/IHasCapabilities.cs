a5a09d440a6c53243598ff55828f6ace17d301f9
Alex Yakunin <alex.yakunin@gmail.com>
2019-08-16 14:27:32 -0700

    Minor change.

--- Stl.Plugins/IHasCapabilities.cs
+++ Stl.Plugins/IHasCapabilities.cs
@@ -1,7 +1,9 @@
 using System.Collections.Immutable;
 
-namespace Stl.Plugins.Extensions
+namespace Stl.Plugins
 {
+    // Implement it in your plugin to support capabilities extraction
+    // and filtering based on capabilities
     public interface IHasCapabilities
     {
         ImmutableDictionary<string, object> Capabilities { get; }
