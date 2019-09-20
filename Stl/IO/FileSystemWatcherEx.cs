9f70b7502c30fa8c86298dca1a1887cefcd089e9
Alex Yakunin <alex.yakunin@gmail.com>
2019-09-20 14:34:42 -0700

    Minor refactoring.

---
+++ Stl/IO/FileSystemWatcherEx.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Reactive.Linq;
+
+namespace Stl.IO
+{
+    public static class FileSystemWatcherEx
+    {
+        public static IObservable<FileSystemEventArgs> ToObservable(this FileSystemWatcher watcher)
+        {
+            var o1 = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+                    handler => {
+                        watcher.Changed += handler;
+                        watcher.Created += handler;
+                        watcher.Deleted += handler;
+                    },
+                    handler => {
+                        watcher.Changed -= handler;
+                        watcher.Created -= handler;
+                        watcher.Deleted -= handler;
+                    })
+                .Select(p => p.EventArgs);
+            var o2 = Observable.FromEventPattern<RenamedEventHandler, RenamedEventArgs>(
+                    handler => {
+                        watcher.Renamed += handler;
+                    },
+                    handler => {
+                        watcher.Renamed -= handler;
+                    })
+                .Select(p => p.EventArgs);
+            return o1.Merge(o2).Publish().RefCount();
+        } 
+    }
+}
