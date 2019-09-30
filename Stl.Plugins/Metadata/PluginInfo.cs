587488a33853dbbf883ebf2c6bcfe3acb24b14e8
Alex Yakunin <alex.yakunin@gmail.com>
2019-09-30 14:38:36 -0700

    Minor fixes.

--- Stl.Plugins/Metadata/PluginInfo.cs
+++ Stl.Plugins/Metadata/PluginInfo.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Newtonsoft.Json;
-using Stl.Plugins.Internal;
 using Stl.Reflection;
 
 namespace Stl.Plugins.Metadata
