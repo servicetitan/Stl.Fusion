6bf63fe4572687ffc79e7f8bb2d4bafeae80b821
Alex Yakunin <alex.yakunin@gmail.com>
2019-10-14 10:21:21 -0700

    Unix related fixes.

--- Stl/OS/OSInfo.cs
+++ Stl/OS/OSInfo.cs
@@ -13,24 +13,18 @@ namespace Stl.OS
     public static class OSInfo
     {
         public static readonly OSKind Kind;
-        public static readonly string HostName;
-        public static readonly string UserName;
         public static readonly string UserHomePath;
 
         static OSInfo()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
                 Kind = OSKind.Windows;
-                HostName = Environment.GetEnvironmentVariable("COMPUTERNAME") ?? "";
-                UserName = Environment.GetEnvironmentVariable("USERNAME") ?? "";
                 UserHomePath = Environment.GetEnvironmentVariable("USERPROFILE") ?? "";
             }
             else {
                 Kind = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) 
                     ? OSKind.MacOS 
                     : OSKind.Unix;
-                HostName = Environment.GetEnvironmentVariable("HOSTNAME") ?? "";
-                UserName = Environment.GetEnvironmentVariable("USER") ?? "";
                 UserHomePath = Environment.GetEnvironmentVariable("HOME") ?? "";
             }
         }
