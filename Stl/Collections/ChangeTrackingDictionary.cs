bb46d37c85ac205157bfddebafc4a1ce1b9cc6c8
Alex Yakunin <alex.yakunin@gmail.com>
2019-11-11 23:41:18 -0800

    WIP w/ freezable models.

--- Stl/Collections/ChangeTrackingDictionary.cs
+++ Stl/Collections/ChangeTrackingDictionary.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -12,18 +13,27 @@ namespace Stl.Collections
         Removed = 2,
     }
 
-    public class ChangeTrackingDictionary<TKey, TValue> : IImmutableDictionary<TKey, TValue>
+    public class ChangeTrackingDictionary<TKey, TValue> : IDictionary<TKey, TValue>
         where TKey : notnull
     {
         public ImmutableDictionary<TKey, TValue> Base { get; }
-        public ImmutableDictionary<TKey, TValue> Dictionary { get; }
-        public ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> Changes { get; }
+        public ImmutableDictionary<TKey, TValue> Dictionary { get; private set;  }
+        public ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> Changes { get; private set; }
 
+        public bool IsReadOnly => false;
         public int Count => Dictionary.Count;
-        public TValue this[TKey key] => Dictionary[key];
-        public IEnumerable<TKey> Keys => Dictionary.Keys;
-        public IEnumerable<TValue> Values => Dictionary.Values;
-        
+        public ICollection<TKey> Keys => throw new NotSupportedException();
+        public ICollection<TValue> Values => throw new NotSupportedException();
+
+        public TValue this[TKey key] {
+            get => Dictionary[key];
+            set {
+                if (!DictionaryUpdate(Dictionary.SetItem(key, value))) 
+                    return;
+                AddUpdate(Changes, key, value);
+            }
+        }
+
         public ChangeTrackingDictionary(
             ImmutableDictionary<TKey, TValue> @base, 
             ImmutableDictionary<TKey, TValue> dictionary = null, 
@@ -39,83 +49,94 @@ namespace Stl.Collections
 
         public bool ContainsKey(TKey key) => Dictionary.ContainsKey(key);
         public bool Contains(KeyValuePair<TKey, TValue> pair) => Dictionary.Contains(pair);
-        public bool TryGetKey(TKey equalKey, out TKey actualKey) => Dictionary.TryGetKey(equalKey, out actualKey);
         public bool TryGetValue(TKey key, out TValue value) => Dictionary.TryGetValue(key, out value);
 
-        IImmutableDictionary<TKey, TValue> IImmutableDictionary<TKey, TValue>.Add(TKey key, TValue value) 
-            => Add(key, value);
-        public ChangeTrackingDictionary<TKey, TValue> Add(TKey key, TValue value)
-            => new ChangeTrackingDictionary<TKey, TValue>(Base, 
-                Dictionary.Add(key, value), 
-                ChangesPlusUpdate(Changes, key, value));
-
-        IImmutableDictionary<TKey, TValue> IImmutableDictionary<TKey, TValue>.AddRange(
-            IEnumerable<KeyValuePair<TKey, TValue>> pairs)
-            => AddRange(pairs);
-        public ChangeTrackingDictionary<TKey, TValue> AddRange(IEnumerable<KeyValuePair<TKey, TValue>> pairs)
+        public void Clear()
         {
-            var newDictionary = Dictionary.AddRange(pairs);
-            var changes = Changes.SetItems(pairs.Select(
-                p => KeyValuePair.Create(p.Key, (GetChangedOrAdded(p.Key), p.Value))));
-            return new ChangeTrackingDictionary<TKey, TValue>(Base, newDictionary, changes);
-        }
-
-        IImmutableDictionary<TKey, TValue> IImmutableDictionary<TKey, TValue>.Remove(TKey key) => Remove(key); 
-        public ChangeTrackingDictionary<TKey, TValue> Remove(TKey key) 
-            => new ChangeTrackingDictionary<TKey, TValue>(Base, 
-                Dictionary.Remove(key), 
-                ChangesPlusRemoval(Changes, key));
-
-        IImmutableDictionary<TKey, TValue> IImmutableDictionary<TKey, TValue>.RemoveRange(IEnumerable<TKey> keys) 
-            => RemoveRange(keys);
-        public ChangeTrackingDictionary<TKey, TValue> RemoveRange(IEnumerable<TKey> keys)
-        {
-            var newDictionary = Dictionary.RemoveRange(keys);
-            var changes = keys.Aggregate(Changes, ChangesPlusRemoval);
-            return new ChangeTrackingDictionary<TKey, TValue>(Base, newDictionary, changes);
-        }
-
-        IImmutableDictionary<TKey, TValue> IImmutableDictionary<TKey, TValue>.SetItem(TKey key, TValue value) 
-            => SetItem(key, value);
-        public ChangeTrackingDictionary<TKey, TValue> SetItem(TKey key, TValue value)
-            => new ChangeTrackingDictionary<TKey, TValue>(Base, 
-                Dictionary.SetItem(key, value), 
-                ChangesPlusUpdate(Changes, key, value));
-
-        IImmutableDictionary<TKey, TValue> IImmutableDictionary<TKey, TValue>.SetItems(
-            IEnumerable<KeyValuePair<TKey, TValue>> items)
-            => SetItems(items);
-        public ChangeTrackingDictionary<TKey, TValue> SetItems(IEnumerable<KeyValuePair<TKey, TValue>> items)
-        {
-            var newDictionary = Dictionary.SetItems(items);
-            var changes = Changes.SetItems(items.Select(
-                p => KeyValuePair.Create(p.Key, (GetChangedOrAdded(p.Key), p.Value))));
-            return new ChangeTrackingDictionary<TKey, TValue>(Base, newDictionary, changes);
-        }
-        
-        IImmutableDictionary<TKey, TValue> IImmutableDictionary<TKey, TValue>.Clear() => Clear(); 
-        public ChangeTrackingDictionary<TKey, TValue> Clear()
-        {
-            var changes = ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)>
+            if (!DictionaryUpdate(ImmutableDictionary<TKey, TValue>.Empty)) 
+                return;
+            Changes = ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)>
                 .Empty
                 .AddRange(Base.Select(
                     p => KeyValuePair.Create(p.Key, (DictionaryEntryChangeType.Removed, p.Value))));
-            return new ChangeTrackingDictionary<TKey, TValue>(Base, ImmutableDictionary<TKey, TValue>.Empty, changes);
         }
 
+        public void Add(KeyValuePair<TKey, TValue> item) 
+            => Add(item.Key, item.Value);
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!DictionaryUpdate(Dictionary.Add(key, value))) 
+                return;
+            Changes = AddUpdate(Changes, key, value);
+        }
+
+        public void AddRange(IEnumerable<KeyValuePair<TKey, TValue>> items)
+        {
+            if (!DictionaryUpdate(Dictionary.AddRange(items))) 
+                return;
+            Changes = Changes.SetItems(items.Select(
+                p => KeyValuePair.Create(p.Key, (GetChangedOrAdded(p.Key), p.Value))));
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            if (!Dictionary.TryGetValue(item.Key, out var v))
+                return false;
+            if (!EqualityComparer<TValue>.Default.Equals(v, item.Value))
+                return false;
+            Dictionary = Dictionary.Remove(item.Key);
+            Changes = AddRemoval(Changes, item.Key);
+            return true;
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (!DictionaryUpdate(Dictionary.Remove(key))) 
+                return false;
+            Changes = AddRemoval(Changes, key);
+            return true;
+        }
+
+        public void RemoveRange(IEnumerable<TKey> keys)
+        {
+            if (!DictionaryUpdate(Dictionary.RemoveRange(keys))) 
+                return;
+            Changes = keys.Aggregate(Changes, AddRemoval);
+        }
+
+        public void SetItems(IEnumerable<KeyValuePair<TKey, TValue>> items)
+        {
+            if (!DictionaryUpdate(Dictionary.SetItems(items))) 
+                return;
+            Changes = Changes.SetItems(items.Select(
+                p => KeyValuePair.Create(p.Key, (GetChangedOrAdded(p.Key), p.Value))));
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) 
+            => Dictionary.ToArray().CopyTo(array, arrayIndex);
+
         // Private / helpers
+
+        private bool DictionaryUpdate(ImmutableDictionary<TKey, TValue> newDictionary)
+        {
+            if (ReferenceEquals(newDictionary, Dictionary))
+                return false;
+            Dictionary = newDictionary;
+            return true;
+        }
 
         private DictionaryEntryChangeType GetChangedOrAdded(TKey key) =>
             Base.ContainsKey(key) 
                 ? DictionaryEntryChangeType.Changed 
                 : DictionaryEntryChangeType.Added;
 
-        private ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> ChangesPlusUpdate(
+        private ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> AddUpdate(
             ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> changes,
             TKey key, TValue value) 
             => changes.SetItem(key, (GetChangedOrAdded(key), value));
 
-        private ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> ChangesPlusRemoval(
+        private ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> AddRemoval(
             ImmutableDictionary<TKey, (DictionaryEntryChangeType ChangeType, TValue Value)> changes,
             TKey key) 
             => Base.TryGetValue(key, out var v)
