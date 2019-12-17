caa18e56a7215b2ec540392183d01e3b7de132f4
Alex Yakunin <alex.yakunin@gmail.com>
2019-12-17 15:51:31 -0800

    Working on TF support in tests (WIP).

--- Stl.CommandLine/ProcessCmdBase.cs
+++ Stl.CommandLine/ProcessCmdBase.cs
@@ -17,12 +17,10 @@ namespace Stl.CommandLine
 
     public abstract class ProcessCmdBase : CmdBase, IProcessCmd
     {
-        protected static readonly ImmutableDictionary<string, string> DefaultEnvironmentVariables =
-            ImmutableDictionary<string, string>.Empty.SetItem("BACH_Activated", "1");
-        
         public PathString Executable { get; }
         public PathString WorkingDirectory { get; set; } = PathString.Empty;
-        public ImmutableDictionary<string, string> EnvironmentVariables { get; set; } = DefaultEnvironmentVariables;
+        public ImmutableDictionary<string, string> EnvironmentVariables { get; set; } = 
+            ImmutableDictionary<string, string>.Empty;
 
         protected ProcessCmdBase(PathString executable) => Executable = executable;
 
