5d809f5a8b3b80c54a03ef36440949607b4d1af6
Alex Yakunin <alex.yakunin@gmail.com>
2020-07-07 02:40:00 -0700

    build: Publication to MyGet

--- build/Publish.ps1
+++ build/Publish.ps1
@@ -1,4 +1,3 @@
-
 function Rebuild() {
     Remove-Item -Recurse artifacts -ErrorAction Ignore
     dotnet build -c Release
@@ -8,11 +7,13 @@ function Rebuild() {
 function Publish($name) {
     $nupkgVersion = nbgv get-version -v NuGetPackageVersion
     dotnet nuget push "artifacts/nupkg/$name.$nupkgVersion.nupkg" -k $env:NUGET_ORG_API_KEY -s https://api.nuget.org/v3/index.json
+    dotnet nuget push "artifacts/nupkg/$name.$nupkgVersion.nupkg" -k $env:MYGET_API_KEY -s $env:MYGET_FEED
 }
 
 function PublishAll() {
     foreach ($package in Get-ChildItem "artifacts/nupkg/*.nupkg" ) {
         dotnet nuget push $package -k $env:NUGET_ORG_API_KEY -s https://api.nuget.org/v3/index.json
+        dotnet nuget push $package -k $env:MYGET_API_KEY -s $env:MYGET_FEED
     }
 }
 
