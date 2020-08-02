783bf162dfd84a4f11fa68bf83aa30d220ca394e
Alex Yakunin <alex.yakunin@gmail.com>
2020-08-01 21:21:40 -0700

    Fix SourceLink - postfix

--- build/Publish.ps1
+++ build/Publish.ps1
@@ -6,18 +6,14 @@ function Rebuild() {
 
 function Publish($name) {
     $nupkgVersion = nbgv get-version -v NuGetPackageVersion
-    dotnet nuget push "artifacts/nupkg/$name.$nupkgVersion.nupkg"  -k $env:NUGET_ORG_API_KEY -s https://api.nuget.org/v3/index.json
-    dotnet nuget push "artifacts/nupkg/$name.$nupkgVersion.snupkg" -k $env:NUGET_ORG_API_KEY -s https://api.nuget.org/v3/index.json
-    dotnet nuget push "artifacts/nupkg/$name.$nupkgVersion.nupkg"  -k $env:MYGET_API_KEY -s $env:MYGET_FEED
+    dotnet nuget push "artifacts/nupkg/$name.$nupkgVersion.nupkg" -k $env:NUGET_ORG_API_KEY -s https://api.nuget.org/v3/index.json
+    dotnet nuget push "artifacts/nupkg/$name.$nupkgVersion.nupkg" -k $env:MYGET_API_KEY -s $env:MYGET_FEED
 }
 
 function PublishAll() {
     foreach ($package in Get-ChildItem "artifacts/nupkg/*.nupkg" ) {
         dotnet nuget push $package -k $env:NUGET_ORG_API_KEY -s https://api.nuget.org/v3/index.json
         dotnet nuget push $package -k $env:MYGET_API_KEY -s $env:MYGET_FEED
-    }
-    foreach ($package in Get-ChildItem "artifacts/nupkg/*.snupkg" ) {
-        dotnet nuget push $package -k $env:NUGET_ORG_API_KEY -s https://api.nuget.org/v3/index.json
     }
 }
 
