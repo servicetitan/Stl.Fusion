278cf8262ed4aea06b88a973a97b677b7b0a6abe
Alex Yakunin <alex.yakunin@gmail.com>
2020-08-06 18:19:01 -0700

    fix: Misc. fixes in the Build.csproj.

--- build/Program.cs
+++ build/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -9,17 +10,16 @@ using System.Threading.Tasks;
 using Bullseye;
 using CliWrap;
 using CliWrap.Buffered;
+using Stl.IO;
 using static Bullseye.Targets;
-
-#pragma warning disable CS1587 // XML comment is not placed on a valid language element
 
 namespace Build
 {
-    // DragonFruit approach doesn't the best option here (for example currently it doesn't support aliases),
-    // but it's clean and works, so I've decided to use it.
+    // DragonFruit isn't the best option here (e.g. currently it doesn't support aliases),
+    // but it does the job.
     internal static class Program
     {
-        /// <summary>Build project for repository</summary>
+        /// <summary>Build project for this repository.</summary>
         /// <param name="arguments">A list of targets to run or list.</param>
         /// <param name="clear">Clear the console before execution.</param>
         /// <param name="dryRun">Do a dry run without executing actions.</param>
@@ -34,7 +34,6 @@ namespace Build
         /// <param name="verbose">Enable verbose output.</param>
         /// <param name="cancellationToken"></param>
         /// <param name="configuration">The configuration for building</param>
-        /// <param name="nugetApiKey">Nuget api key for publishing, also you can use NUGET_ORG_API_KEY env variable</param>
         /// <param name="isPublicRelease">You can redefine PublicRelease property for Nerdbank.GitVersioning</param>
         private static async Task Main(
             string[] arguments,
@@ -50,14 +49,11 @@ namespace Build
             bool skipDependencies,
             bool verbose,
             CancellationToken cancellationToken,
-            // our options here
+            // Our own options
             string configuration = "Debug",
-            string nugetApiKey = "",
-            bool isPublicRelease = true
-            )
+            bool isPublicRelease = true)
         {
-            SetEnvVariables();
-
+            SetDefaults("Stl.Fusion.sln");
             var options = new Options {
                 Clear = clear,
                 DryRun = dryRun,
@@ -72,21 +68,34 @@ namespace Build
                 Verbose = verbose,
             };
 
-            var dotnet = TryFindDotNetExePath()
-                ?? throw new FileNotFoundException("'dotnet' command isn't found. Try to set DOTNET_ROOT variable.");
+            var artifactsPath = PathString.New("artifacts").FullPath;
+            var nupkgPath = artifactsPath & "nupkg";
+            var testOutputPath = artifactsPath & "tests" & "output";
+            var dotnetExePath = TryFindDotNetExePath() ?? throw new FileNotFoundException(
+                "'dotnet' command isn't found. Use DOTNET_ROOT env. var to specify the path to custom 'dotnet' tool.");
 
-            /// for Nerdbank.GitVersioning <seealso href="https://github.com/dotnet/Nerdbank.GitVersioning/blob/master/doc/public_vs_stable.md"/>
-            var publicReleaseEnvVar = Environment.GetEnvironmentVariable("NBGV_PublicRelease");
-            var publicReleaseProperty = $"-p:PublicRelease={(string.IsNullOrWhiteSpace(publicReleaseEnvVar) ? isPublicRelease : bool.Parse(publicReleaseEnvVar))} ";
+            // For Nerdbank.GitVersioning: https://github.com/dotnet/Nerdbank.GitVersioning/blob/master/doc/public_vs_stable.md
+            var isPublicReleaseOverride = bool.TryParse(Environment.GetEnvironmentVariable("NBGV_PublicRelease")?.Trim() ?? "", out var v) ? (bool?) v : null;
+            var publicReleaseProperty = $"-p:PublicRelease={isPublicReleaseOverride ?? isPublicRelease} ";
+
+            Target("clean", () => {
+                DeleteDir(artifactsPath);
+                CreateDir(artifactsPath, true);
+            });
+
+            Target("clean-nupkg", () => {
+                DeleteDir(nupkgPath);
+                CreateDir(nupkgPath, true);
+            });
 
             Target("restore-tools", async () => {
-                await Cli.Wrap(dotnet).WithArguments($"tool restore --ignore-failed-sources")
+                await Cli.Wrap(dotnetExePath).WithArguments($"tool restore --ignore-failed-sources")
                     .ToConsole()
                     .ExecuteAsync(cancellationToken).Task.ConfigureAwait(false);
             });
 
             Target("restore", async () => {
-                await Cli.Wrap(dotnet).WithArguments($"msbuild -noLogo " +
+                await Cli.Wrap(dotnetExePath).WithArguments($"msbuild -noLogo " +
                    "-t:Restore " +
                    "-p:RestoreForce=true " +
                    "-p:RestoreIgnoreFailedSources=True " +
@@ -97,108 +106,70 @@ namespace Build
             });
 
             Target("build", async () => {
-                await Cli.Wrap(dotnet).WithArguments($"build -noLogo -c {configuration} --no-restore {publicReleaseProperty}")
+                await Cli.Wrap(dotnetExePath).WithArguments($"build -noLogo -c {configuration} --no-restore {publicReleaseProperty}")
                     .ToConsole()
                     .ExecuteAsync(cancellationToken).Task.ConfigureAwait(false);
             });
 
             Target("pack", DependsOn("restore", "build"), async () => {
-                await Cli.Wrap(dotnet).WithArguments($"pack -noLogo -c {configuration} --no-build {publicReleaseProperty}")
+                await Cli.Wrap(dotnetExePath).WithArguments($"pack -noLogo -c {configuration} --no-build {publicReleaseProperty}")
                     .ToConsole()
                     .ExecuteAsync(cancellationToken).Task.ConfigureAwait(false);
             });
 
-            Target("nupkg-clean", () => {
-
-                var path = Path.GetFullPath(Path.Combine("artifacts", "nupkg"));
-                if (Directory.Exists(path)) {
-                    try {
-                        Directory.Delete(path, recursive: true);
-                    }
-                    catch { }
-                }
-                Directory.CreateDirectory(path);
-
-            });
-            Target("publish", DependsOn("nupkg-clean", "pack"), async () => {
+            Target("publish", DependsOn("clean-nupkg", "pack"), async () => {
                 const string feed = "https://api.nuget.org/v3/index.json";
-                if (string.IsNullOrWhiteSpace(nugetApiKey)) {
-                    nugetApiKey = Environment.GetEnvironmentVariable("NUGET_ORG_API_KEY") ?? "";
-                }
-                if (string.IsNullOrWhiteSpace(nugetApiKey)) {
-                    throw new Exception("You should specify --nuget-api-key value for publishing");
-                }
-                foreach (var nupkg in Directory.EnumerateFiles(Path.GetFullPath(Path.Combine("artifacts", "nupkg")), "*.nupkg", SearchOption.TopDirectoryOnly)) {
-                    await Cli.Wrap(dotnet).WithArguments($"nuget push \"{nupkg}\" --force-english-output --timeout 60 --api-key \"{nugetApiKey}\" --source \"{feed}\" --skip-duplicate")
+                var nugetOrgApiKey = Environment.GetEnvironmentVariable("NUGET_ORG_API_KEY") ?? "";
+                if (string.IsNullOrWhiteSpace(nugetOrgApiKey))
+                    throw new InvalidOperationException("NUGET_ORG_API_KEY env. var isn't set.");
+                var nupkgPaths = Directory
+                    .EnumerateFiles(nupkgPath.FullPath, "*.nupkg", SearchOption.TopDirectoryOnly)
+                    .Select(PathString.New)
+                    .ToArray();
+                foreach (var nupkgPath in nupkgPaths) {
+                    await Cli.Wrap(dotnetExePath).WithArguments($"nuget push \"{nupkgPath}\" " +
+                            $"--force-english-output " +
+                            $"--timeout 60 " +
+                            $"--api-key \"{nugetOrgApiKey}\" " +
+                            $"--source \"{feed}\" " +
+                            $"--skip-duplicate")
                         .ToConsole()
-                        .ExecuteAsync(cancellationToken).Task.ConfigureAwait(false);
+                        .ExecuteAsync(cancellationToken)
+                        .Task.ConfigureAwait(false);
                 }
             });
 
             Target("coverage", async () => {
-                var resultsDirectory = Path.GetFullPath(Path.Combine("artifacts", "tests", "output"));
-                if (!Directory.Exists(resultsDirectory)) {
-                    try {
-                        Directory.CreateDirectory(resultsDirectory);
-                    }
-                    catch { }
-                }
-                var cmd = await Cli.Wrap(dotnet)
+                CreateDir(testOutputPath);
+                var cmd = await Cli.Wrap(dotnetExePath)
                     .WithArguments($"test " +
-                    "--nologo " +
-                    "--no-restore " +
-                    $"--collect:\"XPlat Code Coverage\" --results-directory {resultsDirectory} " +
-                    $"-c {configuration} " +
-                    "-- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=json,cobertura"
+                        "--nologo " +
+                        "--no-restore " +
+                        $"--collect:\"XPlat Code Coverage\" --results-directory {testOutputPath} " +
+                        $"-c {configuration} " +
+                        "-- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=json,cobertura"
                     )
                     .ToConsole()
-                    .ExecuteBufferedAsync(cancellationToken).Task.ConfigureAwait(false);
+                    .ExecuteBufferedAsync(cancellationToken)
+                    .Task.ConfigureAwait(false);
 
-                MoveAllAttachmentsToResultsDirectory(resultsDirectory, cmd.StandardOutput);
-                TryRemoveTestsOutputDirectories(resultsDirectory);
+                MoveAttachments(testOutputPath, cmd.StandardOutput);
 
-                // Removes all files in inner folders, workaround of https://github.com/microsoft/vstest/issues/2334
-                static void TryRemoveTestsOutputDirectories(string resultsDirectory)
-                {
-                    foreach (var directory in Directory.EnumerateDirectories(resultsDirectory)) {
-                        try {
-                            Directory.Delete(directory, recursive: true);
-                        }
-                        catch { }
-                    }
-                }
-
-                // Removes guid from tests output path, workaround of https://github.com/microsoft/vstest/issues/2378
-                static void MoveAllAttachmentsToResultsDirectory(string resultsDirectory, string output)
-                {
-                    var pattern = $@"Test run for (?<testLib>[^(]*).*?Attachments:(?<filepaths>(?<filepath>[\s]+[^\n]+{Regex.Escape(resultsDirectory)}[^\n]+[\n])+)";
-                    var attachmentsRegex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.CultureInvariant);
-                    foreach (var match in attachmentsRegex.Matches(output).OfType<Match>()) {
-                        var regexPaths = match.Groups["filepaths"].Value.Trim('\n', ' ', '\t', '\r');
-                        var paths = regexPaths.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
-                        if (paths.Length > 0) {
-                            foreach (var path in paths) {
-                                var newFileName = $"{Path.GetFileNameWithoutExtension(match.Groups["testLib"].Value)}.{Path.GetFileName(path)}";
-                                File.Move(path, Path.Combine(resultsDirectory, newFileName), overwrite: true);
-                            }
-                            Directory.Delete(Path.GetDirectoryName(paths[0]), recursive: true);
-                        }
-                    }
-                }
+                // Removes all files in inner folders, workaround for https://github.com/microsoft/vstest/issues/2334
+                foreach (var path in Directory.EnumerateDirectories(testOutputPath).Select(PathString.New))
+                    DeleteDir(path);
             });
 
             Target("default", DependsOn("build"));
 
-
-
             try {
-                /// <see cref="RunTargetsAndExitAsync"/> will hang Target on ctrl+c
+                // RunTargetsAndExitAsync hangs Target on Ctrl+C
                 await RunTargetsWithoutExitingAsync(arguments, options, ex => ex is OperationCanceledException).ConfigureAwait(false);
             }
-            catch (TargetFailedException targetException) {
-                if (targetException.InnerException is OperationCanceledException operationCanceledException) {
+            catch (TargetFailedException tfe) {
+                if (tfe.InnerException is OperationCanceledException oce) {
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(operationCanceledException.Message);
+                    Console.WriteLine(oce.Message);
                     Console.ResetColor();
                 }
             }
@@ -207,44 +178,118 @@ namespace Build
                 Console.WriteLine($"Unhandled exception: {ex}");
                 Console.ResetColor();
             }
+        }
 
+        private static void SetDefaults(string solutionName)
+        {
+            var slnPath = FindNearest(Environment.CurrentDirectory, solutionName)
+                ?? FindNearest(PathString.New(Assembly.GetExecutingAssembly().Location).DirectoryPath, solutionName)
+                ?? throw new InvalidOperationException($"Can't find '{solutionName}'.");
 
-            static void SetEnvVariables()
-            {
-                Environment.SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1");
-                Environment.SetEnvironmentVariable("DOTNET_SVCUTIL_TELEMETRY_OPTOUT", "1");
-                Environment.SetEnvironmentVariable("DOTNET_SKIP_FIRST_TIME_EXPERIENCE", "1");
-                Environment.SetEnvironmentVariable("DOTNET_NOLOGO", "1");
-                Environment.SetEnvironmentVariable("POWERSHELL_TELEMETRY_OPTOUT", "1");
-                Environment.SetEnvironmentVariable("POWERSHELL_UPDATECHECK_OPTOUT", "1");
-                Environment.SetEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en");
+            Environment.CurrentDirectory = slnPath.DirectoryPath;
+            Environment.SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1");
+            Environment.SetEnvironmentVariable("DOTNET_SVCUTIL_TELEMETRY_OPTOUT", "1");
+            Environment.SetEnvironmentVariable("DOTNET_SKIP_FIRST_TIME_EXPERIENCE", "1");
+            Environment.SetEnvironmentVariable("DOTNET_NOLOGO", "1");
+            Environment.SetEnvironmentVariable("POWERSHELL_TELEMETRY_OPTOUT", "1");
+            Environment.SetEnvironmentVariable("POWERSHELL_UPDATECHECK_OPTOUT", "1");
+            Environment.SetEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en");
+        }
+
+        // Removes guid from tests output path, workaround of https://github.com/microsoft/vstest/issues/2378
+        static void MoveAttachments(string coverageOutput, PathString targetPath)
+        {
+            var pattern = $@"Test run for (?<testLib>[^(]*).*?Attachments:(?<filepaths>(?<filepath>[\s]+[^\n]+{Regex.Escape(targetPath)}[^\n]+[\n])+)";
+            var attachmentsRegex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.CultureInvariant);
+            foreach (var match in attachmentsRegex.Matches(coverageOutput).OfType<Match>()) {
+                var regexPaths = match.Groups["filepaths"].Value.Trim('\n', ' ', '\t', '\r');
+                var paths = regexPaths
+                    .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => PathString.New(x.Trim()))
+                    .ToArray();
+                if (paths.Length > 0) {
+                    foreach (var path in paths) {
+                        var newFileName = $"{Path.GetFileNameWithoutExtension(match.Groups["testLib"].Value)}.{path.FileName}";
+                        File.Move(path, targetPath & newFileName, true);
+                    }
+                    DeleteDir(paths[0].DirectoryPath, true);
+                }
             }
         }
 
-        private static string? TryFindDotNetExePath()
+        private static PathString? FindNearest(PathString basePath, string fileName)
         {
-            var dotnet = "dotnet";
+            var path = basePath.ToAbsolute();
+            while (true) {
+                if (File.Exists(path & fileName))
+                    return path & fileName;
+                var nextPath = (path & "..").FullPath;
+                if (nextPath == path)
+                    return null;
+                path = nextPath;
+            }
+        }
+
+
+        private static PathString? TryFindDotNetExePath()
+        {
+            var dotnetExe = "dotnet";
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                dotnet += ".exe";
+                dotnetExe += ".exe";
 
-            var mainModule = Process.GetCurrentProcess().MainModule;
-            if (!string.IsNullOrEmpty(mainModule?.FileName) && Path.GetFileName(mainModule.FileName)!.Equals(dotnet, StringComparison.OrdinalIgnoreCase))
-                return mainModule.FileName;
+            var mainModulePath = PathString.New(Process.GetCurrentProcess().MainModule?.FileName);
+            if (mainModulePath.Value != "" && mainModulePath.FileName.Value.Equals(dotnetExe, StringComparison.OrdinalIgnoreCase))
+                return mainModulePath;
 
-            var environmentVariable = Environment.GetEnvironmentVariable("DOTNET_ROOT");
-            if (!string.IsNullOrEmpty(environmentVariable))
-                return Path.Combine(environmentVariable, dotnet);
+            var dotnetRoot = PathString.New(Environment.GetEnvironmentVariable("DOTNET_ROOT"));
+            if (dotnetRoot.Value != "")
+                return dotnetRoot & dotnetExe;
 
+            return FindInPath(dotnetExe);
+        }
+
+        private static PathString? FindInPath(string fileName)
+        {
             var paths = Environment.GetEnvironmentVariable("PATH");
             if (paths == null)
                 return null;
 
             foreach (var path in paths.Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries)) {
-                var fullPath = Path.Combine(path, dotnet);
+                var fullPath = PathString.New(path) & fileName;
                 if (File.Exists(fullPath))
                     return fullPath;
             }
             return null;
+        }
+
+        private static bool CreateDir(PathString path, bool failOnError = false)
+        {
+            if (Directory.Exists(path))
+                return true;
+            try {
+                Directory.CreateDirectory(path);
+                return true;
+            }
+            catch {
+                if (failOnError)
+                    throw;
+                return false;
+            }
+        }
+
+        private static bool DeleteDir(PathString path, bool failOnError = false)
+        {
+            if (!Directory.Exists(path))
+                return true;
+            try {
+                Directory.Delete(path, true);
+                return true;
+            }
+            catch {
+                if (failOnError)
+                    throw;
+                return false;
+            }
         }
     }
 }
