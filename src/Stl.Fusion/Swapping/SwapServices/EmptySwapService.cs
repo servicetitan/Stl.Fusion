78f1c544811b5dda07724a11b29c63c7e3fba6f4
Alex Yakunin <alex.yakunin@gmail.com>
2020-08-23 03:15:34 -0700

    wip: Add swapping.

--- src/Stl.Fusion/Swapping/SwapServices/EmptySwapService.cs
+++ src/Stl.Fusion/Swapping/SwapServices/EmptySwapService.cs
@@ -5,7 +5,7 @@ using Stl.Fusion.Interception;
 
 namespace Stl.Fusion.Swapping
 {
-    public class FakeSwapService : ISwapService
+    public class EmptySwapService : ISwapService
     {
         public ValueTask<IResult?> LoadAsync((InterceptedInput Input, LTag Version) key, CancellationToken cancellationToken = default)
             => ValueTaskEx.FromResult((IResult?) null);
