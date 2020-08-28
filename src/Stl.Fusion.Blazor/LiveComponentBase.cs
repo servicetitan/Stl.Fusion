19e53f5d9008a6f7a3e10d5a000cc4918112566c
Alex Yakunin <alex.yakunin@gmail.com>
2020-08-28 02:02:47 -0700

    refactor: Further improvements in LiveComponentBase.cs

--- src/Stl.Fusion.Blazor/LiveComponentBase.cs
+++ src/Stl.Fusion.Blazor/LiveComponentBase.cs
@@ -1,10 +1,38 @@
-﻿namespace Stl.Fusion.Blazor
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stl.Fusion.Blazor
 {
     public abstract class LiveComponentBase<T> : StatefulComponentBase<ILiveState<T>>
-    { }
+    {
+        protected override void OnInitialized()
+        {
+            State ??= ServiceProvider.GetService<ILiveState<T>>()
+                ?? StateFactory.NewLive<T>(ConfigureState, ComputeStateAsync, this);
+        }
+
+        protected virtual void ConfigureState(LiveState<T>.Options options) { }
+
+        protected virtual Task<T> ComputeStateAsync(ILiveState<T> state, CancellationToken cancellationToken)
+            // No updates by default
+            => state.AsTask();
+    }
 
     public abstract class LiveComponentBase<T, TOwn> : StatefulComponentBase<ILiveState<T, TOwn>>
     {
         protected IMutableState<TOwn> OwnState => State.OwnState;
+
+        protected override void OnInitialized()
+        {
+            State ??= ServiceProvider.GetService<ILiveState<T, TOwn>>()
+                ?? StateFactory.NewLive<T, TOwn>(ConfigureState, ComputeStateAsync, this);
+        }
+
+        protected virtual void ConfigureState(LiveState<T, TOwn>.Options options) { }
+
+        protected virtual Task<T> ComputeStateAsync(ILiveState<T, TOwn> state, CancellationToken cancellationToken)
+            // No updates by default
+            => state.AsTask();
     }
 }
