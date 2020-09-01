5a5e27964d745c7ed422596e9e3944ba6180950d
Alex Yakunin <alex.yakunin@gmail.com>
2020-09-01 00:58:54 -0700

    doc: Fix some diagrams

--- docs/img-src/Build.ps1
+++ docs/img-src/Build.ps1
@@ -22,6 +22,19 @@ function DockerMmdc($src, $dst, $options)
     Move-Item $dstPath $dst -Force
 }
 
+function NativeMmdc($src, $dst, $options)
+{
+    $dstName = [System.IO.Path]::GetFileName($dst)
+    $dstPath = Join-Path $srcDir $dstName
+    $srcDir = [System.IO.Path]::GetDirectoryName($src)
+    $srcTmpName = "${dstName}.tmp.mmd"
+    $srcTmpPath = Join-Path $srcDir $srcTmpName
+    (Get-Content $src -Raw).Replace("`r`n","`n") | Set-Content $srcTmpPath -Force
+    Invoke-Expression "mmdc -i $srcTmpName -o $dstName $options"
+    Remove-Item $srcTmpPath
+    Move-Item $dstPath $dst -Force
+}
+
 function Build1($src) {
     Write-Host "Processing: $src"
     $magicOptions = "-density 600 -trim +repage -resize 3840 -size 3840x2160 -quality 50"
@@ -36,18 +49,18 @@ function Build1($src) {
     
     # Invoke-Expression "mmdc -i $src -o $dstLightSvg -t neutral"
     # Invoke-Expression "mmdc -i $src -o $dstLightPdf -t neutral -b white"
-    DockerMmdc $src $dstLightSvg "-t neutral"
-    DockerMmdc $src $dstLightPdf "-t neutral -b white"
+    NativeMmdc $src $dstLightSvg "-t neutral"
+    NativeMmdc $src $dstLightPdf "-t neutral -b white"
     Invoke-Expression "magick convert $magicOptions $dstLightPdf $dstLightJpg"
     # Invoke-Expression "mmdc -i $src -o $dstDarkSvg -t dark"
     # Invoke-Expression "mmdc -i $src -o $dstDarkPdf -t dark -b black"
-    DockerMmdc $src $dstDarkSvg "-t dark"
-    DockerMmdc $src $dstDarkPdf "-t dark -b black"
+    NativeMmdc $src $dstDarkSvg "-t dark"
+    NativeMmdc $src $dstDarkPdf "-t dark -b black"
     Invoke-Expression "magick convert $magicOptions $dstDarkPdf $dstDarkJpg"
 }
 
 if ($file -eq "") {
-    # $f = Get-ChildItem ComputedState.mmd
+    # $f = Get-ChildItem .\Computed-Static.mmd
     # Build1 $f.FullName
     Build
 } else {
