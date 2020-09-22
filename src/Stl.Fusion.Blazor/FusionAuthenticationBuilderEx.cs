077891c0555e921eb2e34a185d05ef59d1eb6d59
Alex Yakunin <alex.yakunin@gmail.com>
2020-09-22 02:04:09 -0700

    refactor: A few more improvements in authentication.

--- src/Stl.Fusion.Blazor/FusionAuthenticationBuilderEx.cs
+++ src/Stl.Fusion.Blazor/FusionAuthenticationBuilderEx.cs
@@ -1,31 +1,33 @@
 using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Stl.Fusion.Authentication;
 using Stl.Fusion.Blazor.Authentication;
-using Stl.Fusion.Client;
 
 namespace Stl.Fusion.Blazor
 {
     public static class FusionAuthenticationBuilderEx
     {
-        public static FusionAuthenticationBuilder AddBlazorCore(
+        public static FusionAuthenticationBuilder AddBlazor(
             this FusionAuthenticationBuilder fusionAuth,
             Action<AuthorizationOptions>? configure = null)
         {
             var services = fusionAuth.Services;
             services.AddAuthorizationCore(configure);
+            services.RemoveAll(typeof(AuthenticationStateProvider));
             services.TryAddScoped<AuthenticationStateProvider, AuthStateProvider>();
             services.TryAddTransient(c => (AuthStateProvider) c.GetRequiredService<AuthenticationStateProvider>());
             return fusionAuth;
         }
 
-        public static FusionAuthenticationBuilder AddBlazorClient(
+        public static FusionAuthenticationBuilder AddServerSideBlazor(
             this FusionAuthenticationBuilder fusionAuth,
             Action<AuthorizationOptions>? configure = null)
-            => fusionAuth.AddBlazorCore(configure).AddClient();
+        {
+            fusionAuth.Services.RemoveAll(typeof(AuthenticationStateProvider));
+            return fusionAuth.AddBlazor();
+        }
     }
 }
