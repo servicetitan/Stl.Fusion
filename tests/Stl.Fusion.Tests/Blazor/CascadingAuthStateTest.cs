ce2435e7c02c0b6b0225035e67d78070ea6cf723
Alex Yakunin <alex.yakunin@gmail.com>
2020-11-16 05:19:56 -0800

    fix: Fix one more Blazor-related issue.

---
+++ tests/Stl.Fusion.Tests/Blazor/CascadingAuthStateTest.cs
@@ -0,0 +1,14 @@
+using Stl.Fusion.Blazor.Authentication;
+using Xunit;
+
+namespace Stl.Fusion.Tests.Blazor
+{
+    public class CascadingAuthStateTest
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            var cas = new CascadingAuthState();
+        }
+    }
+}
