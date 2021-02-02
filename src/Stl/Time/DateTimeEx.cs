cfb99ea8e09d1da7d7ca16be9a30a165bc16bd31
Alex Yakunin <alex.yakunin@gmail.com>
2021-02-01 23:40:42 -0800

    refactor: Improve DateTimeEx.

--- src/Stl/Time/DateTimeEx.cs
+++ src/Stl/Time/DateTimeEx.cs
@@ -4,12 +4,25 @@ namespace Stl.Time
 {
     public static class DateTimeEx
     {
-        public static Moment ToMoment(this DateTime source) => new(source);
-        public static Moment ToMoment(this DateTimeOffset source) => new(source);
+        public static Moment ToMoment(this DateTime source)
+            => new(source);
+        public static Moment? ToMoment(this DateTime? source)
+            => source.HasValue ? new Moment(source.GetValueOrDefault()) : null;
+
+        public static Moment ToMoment(this DateTimeOffset source)
+            => new(source);
+        public static Moment? ToMoment(this DateTimeOffset? source)
+            => source.HasValue ? new Moment(source.GetValueOrDefault()) : null;
 
         public static DateTime DefaultKind(this DateTime source, DateTimeKind kind)
             => source.Kind == DateTimeKind.Unspecified
                 ? DateTime.SpecifyKind(source, kind)
                 : source;
+        public static DateTime? DefaultKind(this DateTime? source, DateTimeKind kind)
+            => source.HasValue
+                ? (source.GetValueOrDefault().Kind == DateTimeKind.Unspecified
+                    ? DateTime.SpecifyKind(source.GetValueOrDefault(), kind)
+                    : source)
+                : null;
     }
 }
