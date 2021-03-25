ff2939085b41fc06f0e89a60165362f43013683e
Dmitry.Filippov <Dmitry.Filippov@x-tensive.com>
2021-03-25 18:02:38 +0500

    - make Stl.Testing compilable with some exceptions (code dependent on asp.net core is excluded);

--- src/Stl.Testing/Internal/TestOutputWriter.cs
+++ src/Stl.Testing/Internal/TestOutputWriter.cs
@@ -8,7 +8,10 @@ namespace Stl.Testing.Internal
     public class TestOutputWriter : TextWriter
     {
         protected static readonly string EnvNewLine = Environment.NewLine;
+
+        protected static readonly string LastEnvNewLineString = EnvNewLine[^1].ToString();
         protected static readonly char LastEnvNewLineChar = EnvNewLine[^1];
+
 
         protected StringBuilder Prefix = new();
         public ITestOutputHelper TestOutput { get; }
@@ -30,7 +33,11 @@ namespace Stl.Testing.Internal
             if (value == null)
                 throw new ArgumentNullException(nameof(value));
             Prefix.Append(value);
+            #if NETSTANDARD2_0
+            if (!value.Contains(LastEnvNewLineString))
+            #else
             if (!value.Contains(LastEnvNewLineChar))
+            #endif
                 return;
             var lines = Prefix.ToString().Split(EnvNewLine);
             foreach (var line in lines[..^1])
@@ -40,4 +47,20 @@ namespace Stl.Testing.Internal
         }
     }
 
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public static class RuntimeHelpers
+    {
+        public static T[] GetSubArray<T>(T[] array, System.Range range)
+        {
+            (int offset, int length) = range.GetOffsetAndLength(array.Length);
+            var arr = new T[length];
+            for (int i = 0; i < length; i++) {
+                arr[i] = array[offset + i];
+            }
+            return arr;
+        }
+    }
 }
