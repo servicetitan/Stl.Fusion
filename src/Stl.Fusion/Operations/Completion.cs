815228462063fecca6eb6bda51343cbe73e8a0d6
Alex Yakunin <alex.yakunin@gmail.com>
2021-04-01 02:48:18 -0700

    fix: Fix an issue w/ Completion type initializer.

--- src/Stl.Fusion/Operations/Completion.cs
+++ src/Stl.Fusion/Operations/Completion.cs
@@ -2,6 +2,7 @@ using System;
 using System.Reactive;
 using Stl.CommandR;
 using Stl.CommandR.Commands;
+using Stl.Fusion.Authentication.Commands;
 using Stl.Fusion.Operations.Internal;
 using Stl.Reflection;
 
@@ -29,7 +30,8 @@ namespace Stl.Fusion.Operations
     {
         // This is just to ensure the constructor accepting ICommand is "used",
         // because it is really used inside New, but via reflection.
-        private static readonly Completion<ICommand> DummyCompletion = new(null!);
+        private static readonly Completion<ICommand> DummyCompletion =
+            new(new TransientOperation() { Command = new SignOutCommand() });
 
         public static ICompletion New(IOperation operation)
         {
