1694d38e68c8fa686e6a6a1c073eaa89fad226c7
Dmitry.Filippov <Dmitry.Filippov@x-tensive.com>
2021-04-01 16:42:49 +0500

    - wipe out excessive stuff from WebSocketServer;

--- src/Stl.Fusion.Server.Fx/WebSocketServer.cs
+++ src/Stl.Fusion.Server.Fx/WebSocketServer.cs
@@ -2,12 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Net.WebSockets;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Owin;
@@ -23,27 +19,6 @@ using WebSocketAccept = System.Action<
     System.Func< // WebSocketFunc callback
         System.Collections.Generic.IDictionary<string, object>, // WebSocket environment
         System.Threading.Tasks.Task>>;
-using WebSocketCloseAsync = System.Func<
-    int, // closeStatus
-    string, // closeDescription
-    System.Threading.CancellationToken, // cancel
-    System.Threading.Tasks.Task>;
-using WebSocketReceiveAsync = System.Func<
-    System.ArraySegment<byte>, // data
-    System.Threading.CancellationToken, // cancel
-    System.Threading.Tasks.Task<
-        System.Tuple< // WebSocketReceiveTuple
-            int, // messageType
-            bool, // endOfMessage
-            int>>>; // count
-// closeStatusDescription
-using WebSocketReceiveResult = System.Tuple<int, bool, int>;
-using WebSocketSendAsync = System.Func<
-    System.ArraySegment<byte>, // data
-    int, // message type
-    bool, // end of message
-    System.Threading.CancellationToken, // cancel
-    System.Threading.Tasks.Task>;
 
 namespace Stl.Fusion.Server
 {
@@ -89,7 +64,6 @@ namespace Stl.Fusion.Server
             return env.TryGetValue(key, out value) && value is T ? (T)value : default(T);
         }
         
-        
         public HttpStatusCode HandleRequest(IOwinContext owinContext)
         {
             // written based on https://stackoverflow.com/questions/41848095/websockets-using-owin
@@ -106,7 +80,7 @@ namespace Stl.Fusion.Server
 
             var requestHeaders = GetValue<IDictionary<string, string[]>>(owinContext.Environment, "owin.RequestHeaders");
 
-            Dictionary<string, object> acceptOptions = null;
+            Dictionary<string, object>? acceptOptions = null;
             string[] subProtocols;
             if (requestHeaders.TryGetValue("Sec-WebSocket-Protocol", out subProtocols) && subProtocols.Length > 0) {
                 acceptOptions = new Dictionary<string, object>();
@@ -114,37 +88,12 @@ namespace Stl.Fusion.Server
                 acceptOptions.Add("websocket.SubProtocol", subProtocols[0].Split(',').First().Trim());
             }
 
-            acceptToken(acceptOptions, c => ProcessSocketConnection(c, clientId));
-
+            acceptToken(acceptOptions, wsEnv => {
+                var wsContext = (WebSocketContext)wsEnv["System.Net.WebSockets.WebSocketContext"];
+                return HandleWebSocket(wsContext, clientId);
+            });
 
             return HttpStatusCode.SwitchingProtocols;
-        }
-
-        private Task ProcessSocketConnection(IDictionary<string, object> wsEnv, string clientId)
-        {
-            var wsSendAsync = (WebSocketSendAsync)wsEnv["websocket.SendAsync"];
-            var wsCloseAsync = (WebSocketCloseAsync)wsEnv["websocket.CloseAsync"];
-            var wsCallCancelled = (CancellationToken)wsEnv["websocket.CallCancelled"];
-            var wsRecieveAsync = (WebSocketReceiveAsync)wsEnv["websocket.ReceiveAsync"];
-            var wsContext = (WebSocketContext)wsEnv["System.Net.WebSockets.WebSocketContext"];
-
-            return HandleWebSocket(wsContext, clientId);
-        }
-
-        public async Task HandleRequest(HttpContext context)
-        {
-            if (!context.IsWebSocketRequest) {
-                context.Response.StatusCode = 400;
-                return;
-            }
-            var publisherId = context.Request.QueryString[PublisherIdQueryParameterName];
-            if (Publisher.Id != publisherId) {
-                context.Response.StatusCode = 400;
-                return;
-            }
-
-            var clientId = context.Request.QueryString[ClientIdQueryParameterName];
-            context.AcceptWebSocketRequest(ctx => HandleWebSocket(ctx, clientId));
         }
         
         private async Task HandleWebSocket(WebSocketContext wsContext, string clientId)
@@ -166,7 +115,5 @@ namespace Stl.Fusion.Server
                 Log.LogWarning(e, "WebSocket connection was closed with an error");
             }
         }
-
-        
     }
 }
