4cb83b620501a9d91285301d247cee11600e975f
Alex Yakunin <alex.yakunin@gmail.com>
2021-04-01 01:52:00 -0700

    fix: Minor fix in ParameterComparerAttribute.cs

--- src/Stl.Fusion.Blazor/ParameterComparerAttribute.cs
+++ src/Stl.Fusion.Blazor/ParameterComparerAttribute.cs
@@ -5,7 +5,7 @@ namespace Stl.Fusion.Blazor
     [AttributeUsage(AttributeTargets.Property)]
     public class ParameterComparerAttribute : Attribute
     {
-        public Type ComparerType { get; set; }
+        public Type ComparerType { get; }
 
         public ParameterComparerAttribute(Type comparerType)
             => ComparerType = comparerType;
