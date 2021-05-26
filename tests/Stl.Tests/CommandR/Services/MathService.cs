e712485a9c00f542f7c3ba2e033bc1374cefc2f1
Dmitry.Filippov <Dmitry.Filippov@x-tensive.com>
2021-05-26 17:08:32 +0500

    - extracted RuntimeHelpers.GetSubArray to spearate files;

--- tests/Stl.Tests/CommandR/Services/MathService.cs
+++ tests/Stl.Tests/CommandR/Services/MathService.cs
@@ -64,22 +64,3 @@ namespace Stl.Tests.CommandR.Services
         }
     }
 }
-
-
-#if NET471
-namespace System.Runtime.CompilerServices
-{
-    public static class RuntimeHelpers
-    {
-        public static T[] GetSubArray<T>(T[] array, System.Range range)
-        {
-            (int offset, int length) = range.GetOffsetAndLength(array.Length);
-            var arr = new T[length];
-            for (int i = 0; i < length; i++) {
-                arr[i] = array[offset + i];
-            }
-            return arr;
-        }
-    }
-}
-#endif
