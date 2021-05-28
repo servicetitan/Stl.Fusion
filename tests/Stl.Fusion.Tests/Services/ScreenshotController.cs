57ff497f17c47792e477147518131ec43bbea6d8
crui3er <crui3er@gmail.com>
2021-05-28 18:48:47 +0500

    - unify ScreenshotController for all targets;

--- tests/Stl.Fusion.Tests/Services/ScreenshotController.cs
+++ tests/Stl.Fusion.Tests/Services/ScreenshotController.cs
@@ -2,17 +2,14 @@ using System.Threading.Tasks;
 #if NETCOREAPP
 using Microsoft.AspNetCore.Mvc;
 #else
-using System.Web;
 using System.Web.Http;
+using ControllerBase = System.Web.Http.ApiController;
 #endif
 using Stl.Fusion.Server;
 
 namespace Stl.Fusion.Tests.Services
 {
-    #if NETCOREAPP
-    
-    [Route("api/[controller]/[action]")]
-    [ApiController, JsonifyErrors]
+    [JsonifyErrors]
     public class ScreenshotController : ControllerBase
     {
         public static int CallCount { get; set; }
@@ -24,27 +21,7 @@ namespace Stl.Fusion.Tests.Services
         public Task<Screenshot> GetScreenshot(int width)
         {
             CallCount++;
-            return Service.GetScreenshot(width, HttpContext.RequestAborted);
+            return Service.GetScreenshot(width, this.RequestAborted());
         }
     }
-
-    #else
-    
-    [JsonifyErrors]
-    public class ScreenshotController : ApiController
-    {
-        public static int CallCount { get; set; }
-        protected IScreenshotService Service { get; }
-
-        public ScreenshotController(IScreenshotService service) => Service = service;
-
-        [HttpGet, Publish]
-        public Task<Screenshot> GetScreenshot(int width)
-        {
-            CallCount++;
-            return Service.GetScreenshot(width, ActionContext.RequestAborted());
-        }
-    }
-    
-    #endif
 }
