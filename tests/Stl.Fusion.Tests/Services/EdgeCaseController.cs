a026257e48b3bc81e2647e8040e72297bb1b52b7
crui3er <crui3er@gmail.com>
2021-05-28 18:56:55 +0500

    - unify EdgeCaseController for all targets;

--- tests/Stl.Fusion.Tests/Services/EdgeCaseController.cs
+++ tests/Stl.Fusion.Tests/Services/EdgeCaseController.cs
@@ -3,17 +3,14 @@ using System.Threading.Tasks;
 #if NETCOREAPP
 using Microsoft.AspNetCore.Mvc;
 #else
-using System.Web;
 using System.Web.Http;
+using ControllerBase = System.Web.Http.ApiController;
 #endif
 using Stl.Fusion.Server;
 
 namespace Stl.Fusion.Tests.Services
 {
-    #if NETCOREAPP
-    
-    [Route("api/[controller]/[action]")]
-    [ApiController, JsonifyErrors]
+    [JsonifyErrors]
     public class EdgeCaseController : ControllerBase, IEdgeCaseService
     {
         protected IEdgeCaseService Service { get; }
@@ -25,43 +22,12 @@ namespace Stl.Fusion.Tests.Services
             => Service.GetSuffix(cancellationToken);
 
         [HttpPost]
+#if NETCOREAPP
         public Task SetSuffix([FromQuery] string? suffix, CancellationToken cancellationToken)
-            => Service.SetSuffix(suffix ?? "", cancellationToken);
-
-        [HttpGet, Publish]
-        public Task<string> ThrowIfContainsError(string? source, CancellationToken cancellationToken)
-            => Service.ThrowIfContainsError(source ?? "", cancellationToken);
-
-        [HttpGet, Publish]
-        public Task<string> ThrowIfContainsErrorRewriteErrors(string? source, CancellationToken cancellationToken)
-            => Service.ThrowIfContainsErrorRewriteErrors(source ?? "", cancellationToken);
-
-        [HttpGet, Publish]
-        public Task<string> ThrowIfContainsErrorNonCompute(string? source, CancellationToken cancellationToken)
-            => Service.ThrowIfContainsErrorNonCompute(source ?? "", cancellationToken);
-    }
-
-    [JsonifyErrors(RewriteErrors = true, Order = 1)]
-    public class EdgeCaseRewriteController : EdgeCaseController
-    {
-        public EdgeCaseRewriteController(IEdgeCaseService service) : base(service) { }
-    }
-
-    #else
-    
-    [JsonifyErrors]
-    public class EdgeCaseController : ApiController, IEdgeCaseService
-    {
-        protected IEdgeCaseService Service { get; }
-
-        public EdgeCaseController(IEdgeCaseService service) => Service = service;
-
-        [HttpGet]
-        public Task<string> GetSuffix(CancellationToken cancellationToken)
-            => Service.GetSuffix(cancellationToken);
-
-        [HttpPost]
+#else
+        // TODO: add tests for RestEase calls with different options FromQuery, FromBody, from path segment;
         public Task SetSuffix(string? suffix, CancellationToken cancellationToken)
+#endif        
             => Service.SetSuffix(suffix ?? "", cancellationToken);
 
         [HttpGet, Publish]
@@ -76,14 +42,15 @@ namespace Stl.Fusion.Tests.Services
         public Task<string> ThrowIfContainsErrorNonCompute(string? source, CancellationToken cancellationToken)
             => Service.ThrowIfContainsErrorNonCompute(source ?? "", cancellationToken);
     }
-
-    //[JsonifyErrors(RewriteErrors = true, Order = 1)]
+    
+    #if NETCOREAPP
+    [JsonifyErrors(RewriteErrors = true, Order = 1)]
+    #else
     // It seems there is no concept of filter ordering. Let see what we will get.
     [JsonifyErrors(RewriteErrors = true)]
+    #endif
     public class EdgeCaseRewriteController : EdgeCaseController
     {
         public EdgeCaseRewriteController(IEdgeCaseService service) : base(service) { }
     }
-    
-    #endif
 }
