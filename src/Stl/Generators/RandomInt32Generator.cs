8df126c4c48b04b06723f9fdd03ee20eb880283a
crui3er <crui3er@gmail.com>
2021-05-31 17:23:49 +0500

    - use new implementation for RandomInt32Generator and RandomInt64Generator. It should be good as old one, but it suits for all targets.

--- src/Stl/Generators/RandomInt32Generator.cs
+++ src/Stl/Generators/RandomInt32Generator.cs
@@ -4,23 +4,6 @@ using System.Security.Cryptography;
 
 namespace Stl.Generators
 {
-#if !NETSTANDARD2_0
-    public sealed class RandomInt32Generator : Generator<long>
-    {
-        private readonly int[] _buffer = new int[1];
-        private readonly RandomNumberGenerator _rng;
-
-        public RandomInt32Generator(RandomNumberGenerator? rng = null)
-            => _rng = rng ?? RandomNumberGenerator.Create();
-
-        public override long Next()
-        {
-            var bufferSpan = MemoryMarshal.Cast<int, byte>(_buffer.AsSpan());
-            _rng!.GetBytes(bufferSpan);
-            return _buffer![0];
-        }
-    }
-#else
     public sealed class RandomInt32Generator : Generator<long>
     {
         private readonly byte[] _buffer = new byte[sizeof(int)];
@@ -36,5 +19,4 @@ namespace Stl.Generators
             return bufferSpan![0];
         }
     }
-#endif
 }
--- src/Stl/Generators/RandomInt64Generator.cs
+++ src/Stl/Generators/RandomInt64Generator.cs
@@ -4,23 +4,6 @@ using System.Security.Cryptography;
 
 namespace Stl.Generators
 {
-#if !NETSTANDARD2_0
-    public sealed class RandomInt64Generator : Generator<long>
-    {
-        private readonly long[] _buffer = new long[1];
-        private readonly RandomNumberGenerator _rng;
-
-        public RandomInt64Generator(RandomNumberGenerator? rng = null)
-            => _rng = rng ?? RandomNumberGenerator.Create();
-
-        public override long Next()
-        {
-            var bufferSpan = MemoryMarshal.Cast<long, byte>(_buffer.AsSpan());
-            _rng!.GetBytes(bufferSpan);
-            return _buffer![0];
-        }
-    }
-#else
     public sealed class RandomInt64Generator : Generator<long>
     {
         private readonly byte[] _buffer = new byte[sizeof(long)];
@@ -36,5 +19,4 @@ namespace Stl.Generators
             return bufferSpan![0];
         }
     }
-#endif
 }
