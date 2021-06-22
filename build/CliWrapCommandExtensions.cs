1bca5b3d2bc4a8002106274d442d081612e26e65
Alexey Golub <tyrrrrrr@gmail.com>
2021-06-22 10:06:30 -0700

    Simplify CliWrap usage (#296)
    * Simplify CliWrap usage
    
    * Try to fix build error

--- build/CliWrapCommandExtensions.cs
+++ build/CliWrapCommandExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using CliWrap;
+using CliWrap.Builders;
 
 namespace Build
 {
@@ -10,6 +11,10 @@ namespace Build
         internal static Stream _stderr = Console.OpenStandardError();
 
         internal static Command ToConsole(this Command command) => command | (_stdout, _stderr);
+
+        internal static ArgumentsBuilder AddOption(this ArgumentsBuilder args, string name, string value) =>
+            !string.IsNullOrEmpty(value)
+                ? args.Add(name).Add(value)
+                : args;
     }
 }
-
