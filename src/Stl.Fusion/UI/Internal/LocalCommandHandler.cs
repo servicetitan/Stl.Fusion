82ac914afa8933c9bfff5592ae8c982d634940bb
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-01 05:01:24 -0700

    fix: LocalCommandHandler

--- src/Stl.Fusion/UI/Internal/LocalCommandHandler.cs
+++ src/Stl.Fusion/UI/Internal/LocalCommandHandler.cs
@@ -1,14 +1,16 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Stl.CommandR;
+using Stl.CommandR.Configuration;
 
 namespace Stl.Fusion.UI.Internal
 {
-    public class LocalCommandHandler
+    public class LocalCommandHandler : ICommandHandler<LocalCommand>
     {
+        [CommandHandler(Priority = 900_000_000, IsFilter = false)]
         public Task OnCommand(
             LocalCommand command, CommandContext context,
             CancellationToken cancellationToken)
-            => command.Handler?.Invoke() ?? Task.CompletedTask;
+            => command.Handler!.Invoke();
     }
 }
