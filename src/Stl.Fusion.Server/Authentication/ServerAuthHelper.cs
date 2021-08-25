d9bba7ecc8011710d5c4a698a9758c062b835f41
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-25 03:23:05 -0700

    fix: too frequent SessionInfo updates

--- src/Stl.Fusion.Server/Authentication/ServerAuthHelper.cs
+++ src/Stl.Fusion.Server/Authentication/ServerAuthHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -11,6 +12,7 @@ using Stl.Fusion.Authentication;
 using Stl.Fusion.Authentication.Commands;
 using Stl.Fusion.Server.Internal;
 using Stl.Text;
+using Stl.Time;
 
 namespace Stl.Fusion.Server.Authentication
 {
@@ -21,30 +23,37 @@ namespace Stl.Fusion.Server.Authentication
             public string[] IdClaimKeys { get; set; } = { ClaimTypes.NameIdentifier };
             public string[] NameClaimKeys { get; set; } = { ClaimTypes.Name };
             public string CloseWindowRequestPath { get; set; } = "/fusion/close";
+            public TimeSpan SessionInfoUpdatePeriod { get; set; } = TimeSpan.FromSeconds(30);
         }
 
         protected IServerSideAuthService AuthService { get; }
         protected ISessionResolver SessionResolver { get; }
         protected AuthSchemasCache AuthSchemasCache { get; }
+        protected MomentClockSet Clocks { get; }
+
         public string[] IdClaimKeys { get; }
         public string[] NameClaimKeys { get; }
         public string CloseWindowRequestPath { get; }
+        public TimeSpan SessionInfoUpdatePeriod { get; }
         public Session Session => SessionResolver.Session;
 
         public ServerAuthHelper(
             Options? options,
             IServerSideAuthService authService,
             ISessionResolver sessionResolver,
-            AuthSchemasCache authSchemasCache)
+            AuthSchemasCache authSchemasCache,
+            MomentClockSet clocks)
         {
             options ??= new();
             IdClaimKeys = options.IdClaimKeys;
             NameClaimKeys = options.NameClaimKeys;
             CloseWindowRequestPath = options.CloseWindowRequestPath;
+            SessionInfoUpdatePeriod = options.SessionInfoUpdatePeriod;
 
             AuthService = authService;
             SessionResolver = sessionResolver;
             AuthSchemasCache = authSchemasCache;
+            Clocks = clocks;
         }
 
         public virtual async ValueTask<string> GetSchemas(HttpContext httpContext, bool cache = true)
@@ -78,8 +87,18 @@ namespace Stl.Fusion.Server.Authentication
             var userAgent = httpContext.Request.Headers.TryGetValue("User-Agent", out var userAgentValues)
                 ? userAgentValues.FirstOrDefault() ?? ""
                 : "";
-            var setupSessionCommand = new SetupSessionCommand(session, ipAddress, userAgent).MarkServerSide();
-            var sessionInfo = await AuthService.SetupSession(setupSessionCommand, cancellationToken).ConfigureAwait(false);
+
+            var sessionInfo = await AuthService.GetSessionInfo(session, cancellationToken).ConfigureAwait(false);
+            var mustUpdateSessionInfo =
+                sessionInfo.IsAuthenticated != isAuthenticated
+                || sessionInfo.IPAddress != ipAddress
+                || sessionInfo.UserAgent != userAgent
+                || sessionInfo.LastSeenAt.ToMoment() + SessionInfoUpdatePeriod < Clocks.SystemClock.Now;
+            if (mustUpdateSessionInfo) {
+                var setupSessionCommand = new SetupSessionCommand(session, ipAddress, userAgent).MarkServerSide();
+                sessionInfo = await AuthService.SetupSession(setupSessionCommand, cancellationToken).ConfigureAwait(false);
+            }
+
             var userId = sessionInfo.UserId;
             var userIsAuthenticated = sessionInfo.IsAuthenticated && !sessionInfo.IsSignOutForced;
             var user = userIsAuthenticated
