f4799ad9fc539da87fd663a069f04b7630059c9f
Alex Yakunin <alex.yakunin@gmail.com>
2022-05-13 01:14:32 -0700

    feat: make fusionAuth.js to fallback to redirect when authentication popup is blocked or use an alert to notify about this

--- src/Stl.Fusion.Blazor/wwwroot/scripts/fusionAuth.js
+++ src/Stl.Fusion.Blazor/wwwroot/scripts/fusionAuth.js
@@ -11,6 +11,7 @@ window.FusionAuth = {
     signInPath: "/signIn",
     signOutPath: "/signOut",
     closePath: "/fusion/close",
+    mustRedirectOnPopupBlock: true,
 
     signIn: function (schema) {
         if (schema === undefined || schema === null || schema === "") {
@@ -28,7 +29,18 @@ window.FusionAuth = {
         let encode = encodeURIComponent;
         let redirectUrl = new URL(this.closePath +"?flow=" + encode(flowName), document.baseURI).href;
         let url = action + "?returnUrl=" + encode(redirectUrl);
-        return window.open(url, FusionAuth.windowTarget, FusionAuth.windowFeatures);
+        let popup = window.open(url, FusionAuth.windowTarget, FusionAuth.windowFeatures);
+        // let popup = { closed: true };
+        if (!popup || popup.closed || typeof popup.closed=='undefined') {
+            if (this.mustRedirectOnPopupBlock) {
+                let redirectUrl = window.location.href;
+                let url = action + "?returnUrl=" + encode(redirectUrl);
+                window.location.replace(url);
+            }
+            else {
+                alert("Authentication popup is blocked by the browser. Please allow popups on this website and retry.")
+            }
+        }        
     }
 };
 
