e7b639a52625d65ad832501f1ec6f262a9c4b33f
Alex Yakunin <alex.yakunin@gmail.com>
2022-07-03 13:54:04 -0700

    chore: sync samples

--- samples/TodoApp/UI/Program.cs
+++ samples/TodoApp/UI/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Stl.OS;
 using Stl.DependencyInjection;
 
 namespace Templates.TodoApp.UI;
@@ -8,13 +7,10 @@ public class Program
 {
     public static Task Main(string[] args)
     {
-        if (OSInfo.Kind != OSKind.WebAssembly)
-            throw new ApplicationException("This app runs only in browser.");
-
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         StartupHelper.ConfigureServices(builder.Services, builder);
         var host = builder.Build();
-        host.Services.HostedServices().Start();
+        _ = host.Services.HostedServices().Start();
         return host.RunAsync();
     }
 }
