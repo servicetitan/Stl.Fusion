c55885c7f920f7e87ccc385c8e60ae92c869afef
Alex Yakunin <alex.yakunin@gmail.com>
2022-07-10 18:08:57 -0700

    chore: argument rename

--- src/Stl.Fusion/ExceptionExt.cs
+++ src/Stl.Fusion/ExceptionExt.cs
@@ -5,6 +5,6 @@ public static class ExceptionExt
     public static ResultException ToResult(this Exception wrappedException)
         => new(wrappedException.Message, wrappedException);
 
-    public static Exception MaybeToResult(this Exception error, bool returnResultException)
-        => returnResultException ? error.ToResult() : error;
+    public static Exception MaybeToResult(this Exception sourceException, bool mustWrap)
+        => mustWrap ? sourceException.ToResult() : sourceException;
 }
