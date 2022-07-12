c8e1e20e9f4af76946d91e620089e34c05c71d41
Alex Yakunin <alex.yakunin@gmail.com>
2022-07-11 22:29:31 -0700

    fix: ToResultException was broken

--- src/Stl.Fusion/ExceptionExt.cs
+++ src/Stl.Fusion/ExceptionExt.cs
@@ -5,7 +5,7 @@ public static class ExceptionExt
     public static Exception ToResultException(this Exception wrappedException) 
         => wrappedException is OperationCanceledException 
             ? wrappedException 
-            : new(wrappedException.Message, wrappedException);
+            : new ResultException(wrappedException.Message, wrappedException);
 
     public static Exception MaybeToResultException(this Exception sourceException, bool wrapToResultException)
         => wrapToResultException ? sourceException.ToResultException() : sourceException;
