9bfbb636361eeb6dfe9113ef65c36d4afa669c79
Alex Yakunin <alex.yakunin@gmail.com>
2022-07-26 09:52:31 -0700

    refactor: add TransientException

--- src/Stl/ITransientException.cs
+++
@@ -1,7 +0,0 @@
-namespace Stl;
-
-/// <summary>
-/// A tagging interface for any exception that might be "cured" by retrying the operation.
-/// </summary>
-public interface ITransientException
-{ }
---
+++ src/Stl/TransientException.cs
@@ -0,0 +1,19 @@
+namespace Stl;
+
+/// <summary>
+/// A tagging interface for any exception that might be "cured" by retrying the operation.
+/// </summary>
+public interface ITransientException
+{ }
+
+[Serializable]
+public class TransientException : Exception, ITransientException
+{
+    public TransientException() : this(null) { }
+    public TransientException(string? message)
+        : base(message ?? "Transient error.") { }
+    public TransientException(string? message, Exception innerException)
+        : base(message ?? "Transient error.", innerException) { }
+    protected TransientException(SerializationInfo info, StreamingContext context) 
+        : base(info, context) { }
+}
