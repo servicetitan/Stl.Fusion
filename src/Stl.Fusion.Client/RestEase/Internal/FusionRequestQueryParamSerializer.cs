99708c0ecc75c94118891d5ad0d71ebdcb58df16
Alex Yakunin <alex.yakunin@gmail.com>
2022-08-22 22:47:24 -0700

    refactor: improve FusionRequestQueryParamSerializer.cs

--- src/Stl.Fusion.Client/RestEase/Internal/FusionRequestQueryParamSerializer.cs
+++ src/Stl.Fusion.Client/RestEase/Internal/FusionRequestQueryParamSerializer.cs
@@ -1,5 +1,7 @@
 using System.Globalization;
 using RestEase;
+using Stl.Fusion.Authentication;
+using Stl.Fusion.Extensions;
 
 namespace Stl.Fusion.Client.RestEase.Internal;
 
@@ -10,49 +12,76 @@ namespace Stl.Fusion.Client.RestEase.Internal;
 /// </summary>
 public class FusionRequestQueryParamSerializer : RequestQueryParamSerializer
 {
-        public override IEnumerable<KeyValuePair<string, string?>> SerializeQueryParam<T>(
-            string name, T value, RequestQueryParamSerializerInfo info)
-            => Serialize(name, value, info);
+    public override IEnumerable<KeyValuePair<string, string?>> SerializeQueryParam<T>(
+        string name, T value, RequestQueryParamSerializerInfo info)
+        => Serialize(name, value, info);
 
-        public override IEnumerable<KeyValuePair<string, string?>> SerializeQueryCollectionParam<T>(
-            string name, IEnumerable<T> values, RequestQueryParamSerializerInfo info)
-            => Serialize(name, values, info);
+    public override IEnumerable<KeyValuePair<string, string?>> SerializeQueryCollectionParam<T>(
+        string name, IEnumerable<T> values, RequestQueryParamSerializerInfo info)
+        => Serialize(name, values, info);
 
-        private IEnumerable<KeyValuePair<string, string?>> Serialize<T>(string name, T value, RequestQueryParamSerializerInfo info)
-        {
-            if (value == null)
-                yield break;
-
-            foreach (var p in BuildPropertyMap(value, name)) {
-                var sValue = p.Value switch {
-                    null => "",
-                    DateTime dateTime => dateTime.ToString(info.Format ?? "o", CultureInfo.InvariantCulture),
-                    _ => p.Value?.ToString() ?? "",
-                };
-                yield return new KeyValuePair<string, string?>(p.Key, sValue);
-            }
+    protected IEnumerable<KeyValuePair<string, string?>> Serialize<T>(
+        string name, T value, 
+        RequestQueryParamSerializerInfo info)
+    {
+        if (ReferenceEquals(value, null))
+            yield break;
+        var sValue = SerializeSimpleType(value, info);
+        if (sValue != null) {
+            yield return new KeyValuePair<string, string?>(name, sValue);
+            yield break;
         }
+        foreach (var (key, sValue1) in SerializeComplexType(name, value, info))
+            yield return new KeyValuePair<string, string?>(key, sValue1);
+    }
 
-        private Dictionary<string, object?> BuildPropertyMap(
-            object? source, string name, 
-            Dictionary<string, object?>? map = null)
-        {
-            map ??= new Dictionary<string, object?>(StringComparer.Ordinal);
+    protected virtual string? SerializeSimpleType(
+        object source,
+        RequestQueryParamSerializerInfo info)
+    {
+        if (source is string or Session or PageRef)
+            return source.ToString() ?? "";
+        if (source.GetType().IsValueType)
+            return source is DateTime dateTime
+                ? dateTime.ToString(info.Format ?? "o", CultureInfo.InvariantCulture)
+                : source.ToString() ?? "";
+        return null;
+    }
 
-            if (ReferenceEquals(source, null))
-                map.Add(name, null);
-            else if (source.GetType().IsValueType || source is string)
-                map.Add(name, source);
-            else if (source is IEnumerable sequence) {
-                var i = 0;
-                foreach (var item in sequence)
-                    BuildPropertyMap(item, $"{name}[{i++}]", map);
-            }
-            else {
-                var prefix = name.IsNullOrEmpty() ? "" : $"{name}.";
-                foreach (var p in source.GetType().GetProperties())
-                    BuildPropertyMap(p.GetValue(source, null), $"{prefix}{p.Name}", map);
-            }
+    protected virtual Dictionary<string, string> SerializeComplexType(
+        string name, object? value,
+        RequestQueryParamSerializerInfo info,
+        Dictionary<string, string>? map = null)
+    {
+        map ??= new Dictionary<string, string>(StringComparer.Ordinal);
+
+        // Null
+        if (ReferenceEquals(value, null)) {
+            map.Add(name, "");
             return map;
         }
+
+        // Simple type
+        var serialized = SerializeSimpleType(value, info);
+        if (serialized != null) {
+            map.Add(name, serialized);
+            return map;
+        }
+
+        // Collection
+        if (value is IEnumerable collection) {
+            var index = 0;
+            foreach (var item in collection)
+                SerializeComplexType($"{name}[{index++}]", item, info, map);
+            return map;
+        }
+
+        // Complex type
+        var prefix = name.IsNullOrEmpty() ? "" : $"{name}.";
+        foreach (var property in value.GetType().GetProperties()) {
+            var pValue = property.GetValue(value, null);
+            SerializeComplexType($"{prefix}{property.Name}", pValue, info, map);
+        }
+        return map;
     }
+}
--- src/Stl.Fusion/Extensions/PageRef.cs
+++ src/Stl.Fusion/Extensions/PageRef.cs
@@ -2,7 +2,22 @@ using System.Globalization;
 
 namespace Stl.Fusion.Extensions;
 
-public record PageRef<TKey>(int Count, Option<TKey> After = default)
+public abstract record PageRef
+{
+    public static PageRef<TKey> New<TKey>(int count)
+        => new(count);
+    public static PageRef<TKey> New<TKey>(int count, TKey after)
+        => new(count, Option.Some(after));
+    public static PageRef<TKey> New<TKey>(int count, Option<TKey> after)
+        => new(count, after);
+
+    public static PageRef<TKey> Parse<TKey>(string value)
+        => int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var count)
+            ? new PageRef<TKey>(count)
+            : SystemJsonSerializer.Default.Read<PageRef<TKey>>(value);
+}
+
+public record PageRef<TKey>(int Count, Option<TKey> After = default) : PageRef
 {
     public PageRef() : this(0) { }
 
@@ -17,19 +32,4 @@ public record PageRef<TKey>(int Count, Option<TKey> After = default)
         => new(source.Count, Option.Some(source.AfterKey));
     public static implicit operator PageRef<TKey>((int Count, Option<TKey> AfterKey) source)
         => new(source.Count, source.AfterKey);
-}
-
-public static class PageRef
-{
-    public static PageRef<TKey> New<TKey>(int count)
-        => new(count);
-    public static PageRef<TKey> New<TKey>(int count, TKey after)
-        => new(count, Option.Some(after));
-    public static PageRef<TKey> New<TKey>(int count, Option<TKey> after)
-        => new(count, after);
-
-    public static PageRef<TKey> Parse<TKey>(string value)
-        => int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var count)
-            ? new PageRef<TKey>(count)
-            : SystemJsonSerializer.Default.Read<PageRef<TKey>>(value);
 }
