ad3156c1293a84b83115067747bf9c509e111e6f
Alex Yakunin <alex.yakunin@gmail.com>
2023-04-20 18:22:58 -0700

    feat: CpuTimestamp - Stopwatch with zero allocations

---
+++ src/Stl/Time/CpuTimestamp.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+
+namespace Stl.Time;
+
+public record struct CpuTimestamp(long Value) : IComparable<CpuTimestamp>
+{
+    public static readonly long TicksPerSecond = 10_000_000;
+    public static readonly double SecondsPerTick;
+
+    static CpuTimestamp()
+    {
+        var tickFrequency = typeof(Stopwatch)
+            .GetField("s_tickFrequency",
+                BindingFlags.Static | BindingFlags.NonPublic)
+            ?.GetValue(null);
+        if (tickFrequency is double tf)
+            TicksPerSecond = (long)tf * Stopwatch.Frequency;
+        SecondsPerTick = 1d / TicksPerSecond;
+    }
+
+    public static CpuTimestamp Now {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new(Stopwatch.GetTimestamp());
+    }
+
+    public override string ToString()
+        => Value + " ticks";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TimeSpan ElapsedFrom(CpuTimestamp other)
+        => Now - other;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TimeSpan operator -(CpuTimestamp a, CpuTimestamp b)
+        => TimeSpan.FromSeconds(SecondsPerTick * (a.Value - b.Value));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int CompareTo(CpuTimestamp other)
+        => Value.CompareTo(other.Value);
+}
---
+++ tests/Stl.Tests/Time/CpuTimestampTest.cs
@@ -0,0 +1,18 @@
+namespace Stl.Tests.Time;
+
+public class CpuTimestampTest : TestBase
+{
+    public CpuTimestampTest(ITestOutputHelper @out) : base(@out) { }
+
+    [Fact]
+    public async Task BasicTest()
+    {
+        CpuTimestamp.TicksPerSecond.Should().Be(10_000_000);
+        var now = CpuTimestamp.Now;
+        await Task.Delay(100);
+        CpuTimestamp.ElapsedFrom(now).Should().BeGreaterThan(TimeSpan.FromMilliseconds(50));
+        await Task.Delay(100);
+        CpuTimestamp.ElapsedFrom(now).Should().BeGreaterThan(TimeSpan.FromMilliseconds(150));
+        CpuTimestamp.ElapsedFrom(now).Should().BeLessThan(TimeSpan.FromMilliseconds(2000));
+    }
+}
