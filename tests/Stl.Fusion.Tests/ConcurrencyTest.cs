8faf9d9a78071378aed112b82bb04644dd386639
Alex Yakunin <alex.yakunin@gmail.com>
2023-04-21 17:02:23 -0700

    chore: minor change in ConcurrencyTest.cs

--- tests/Stl.Fusion.Tests/ConcurrencyTest.cs
+++ tests/Stl.Fusion.Tests/ConcurrencyTest.cs
@@ -195,7 +195,7 @@ public class ConcurrencyTest : SimpleFusionTestBase
                 foreach (var reader in readers) {
                     var c = reader.Computed;
                     if (c.Value != expectedValue) {
-                        await c.WhenInvalidated().WaitAsync(TimeSpan.FromSeconds(0.5));
+                        await c.WhenInvalidated().WaitAsync(TimeSpan.FromSeconds(1));
                         c = await c.Update();
                     }
                     if (c.Value != expectedValue) {
