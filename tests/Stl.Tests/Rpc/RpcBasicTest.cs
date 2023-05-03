30c6f0e77a284089643171ded1e13191198e5f59
Alex Yakunin <alex.yakunin@gmail.com>
2023-05-03 07:44:59 -0700

    chore: minor changes

--- tests/Stl.Tests/Rpc/RpcBasicTest.cs
+++ tests/Stl.Tests/Rpc/RpcBasicTest.cs
@@ -20,8 +20,4 @@ public class RpcBasicTest : TestBase
         rpc.AddService<ISimpleRpcService, SimpleRpcService>();
         return services.BuildServiceProvider();
     }
-
-    // Nested types
-
 }
-
