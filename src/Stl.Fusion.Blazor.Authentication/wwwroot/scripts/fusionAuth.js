88bf70528d66dc8488a241d25621809f9ae43dea
Alex Yakunin <alex.yakunin@gmail.com>
2023-06-14 23:58:09 -0700

    feat: fusionAuth.js - allow to disable popup completely

--- src/Stl.Fusion.Blazor.Authentication/wwwroot/scripts/fusionAuth.js
+++ src/Stl.Fusion.Blazor.Authentication/wwwroot/scripts/fusionAuth.js
@@ -1,47 +1,53 @@
 (function() {
 
-let openAuthWindow = function (action, flowName) {
-    return FusionAuth.openAuthWindow(action, flowName);
-}
+    let encode = encodeURIComponent;
 
-window.FusionAuth = {
-    schemas: "",
-    windowTarget: "_blank",
-    windowFeatures: "width=600,height=600",
-    signInPath: "/signIn",
-    signOutPath: "/signOut",
-    closePath: "/fusion/close",
-    mustRedirectOnPopupBlock: true,
+    window.FusionAuth = {
+        schemas: "",
+        windowTarget: "_blank",
+        windowFeatures: "width=600,height=600",
+        signInPath: "/signIn",
+        signOutPath: "/signOut",
+        closePath: "/fusion/close",
+        enablePopup: true,
+        mustRedirectOnPopupBlock: true,
 
-    signIn: function (schema) {
-        if (schema === undefined || schema === null || schema === "") {
-            openAuthWindow(this.signInPath, "Sign-in");
-        } else {
-            openAuthWindow(this.signInPath + "/" + schema, "Sign-in");
+        signIn: function(schema) {
+            if (schema === undefined || schema === null || schema === "") {
+                this.authPopupOrRedirect(this.signInPath, "Sign-in");
+            } else {
+                this.authPopupOrRedirect(this.signInPath + "/" + schema, "Sign-in");
+            }
+        },
+
+        signOut: function() {
+            this.authPopupOrRedirect(this.signOutPath, "Sign-out");
+        },
+
+        authPopupOrRedirect: function(action, flowName) {
+            if (!this.enablePopup) {
+                this.authRedirect(action);
+                return;
+            }
+
+            let redirectUrl = new URL(this.closePath +"?flow=" + encode(flowName), document.baseURI).href;
+            let url = action + "?returnUrl=" + encode(redirectUrl);
+            let popup = window.open(url, FusionAuth.windowTarget, FusionAuth.windowFeatures);
+            if (!popup || popup.closed || typeof popup.closed == 'undefined') {
+                if (this.mustRedirectOnPopupBlock) {
+                    this.authRedirect(action);
+                }
+                else {
+                    alert("Authentication popup is blocked by the browser. Please allow popups on this website and retry.")
+                }
+            }
+        },
+
+        authRedirect(action) {
+            let redirectUrl = window.location.href;
+            let url = action + "?returnUrl=" + encode(redirectUrl);
+            window.location.replace(url);
         }
-    },
-
-    signOut: function () {
-        openAuthWindow(this.signOutPath, "Sign-out");
-    },
-
-    openAuthWindow: function (action, flowName) {
-        let encode = encodeURIComponent;
-        let redirectUrl = new URL(this.closePath +"?flow=" + encode(flowName), document.baseURI).href;
-        let url = action + "?returnUrl=" + encode(redirectUrl);
-        let popup = window.open(url, FusionAuth.windowTarget, FusionAuth.windowFeatures);
-        // let popup = { closed: true };
-        if (!popup || popup.closed || typeof popup.closed=='undefined') {
-            if (this.mustRedirectOnPopupBlock) {
-                let redirectUrl = window.location.href;
-                let url = action + "?returnUrl=" + encode(redirectUrl);
-                window.location.replace(url);
-            }
-            else {
-                alert("Authentication popup is blocked by the browser. Please allow popups on this website and retry.")
-            }
-        }        
-    }
-};
+    };
 
 })();
