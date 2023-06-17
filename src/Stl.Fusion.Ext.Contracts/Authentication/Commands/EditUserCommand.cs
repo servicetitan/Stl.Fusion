98bc91e589539a4cc72d844ef91b465615f6796f
Alex Yakunin <alex.yakunin@gmail.com>
2023-06-16 19:43:05 -0700

    wip: MemoryPack support

--- src/Stl.Fusion.Ext.Contracts/Authentication/Commands/EditUserCommand.cs
+++ src/Stl.Fusion.Ext.Contracts/Authentication/Commands/EditUserCommand.cs
@@ -3,12 +3,15 @@ using MemoryPack;
 namespace Stl.Fusion.Authentication.Commands;
 
 [DataContract, MemoryPackable(GenerateType.VersionTolerant)]
-public partial record EditUserCommand(
-    [property: DataMember, MemoryPackOrder(0)] Session Session
-    ) : ISessionCommand<Unit>
+public partial record EditUserCommand : ISessionCommand<Unit>
 {
+    [DataMember, MemoryPackOrder(0)]
+    public Session Session { get; init; } = Session.Null;
+
     [DataMember, MemoryPackOrder(1)]
     public string? Name { get; init; }
+
+    public EditUserCommand() { }
 
     [MemoryPackConstructor]
     public EditUserCommand(Session session, string? name = null) : this(session)
