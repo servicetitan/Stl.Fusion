95b204a1f4a60fbcea0810f035df2152c0f2d83b
Alex Yakunin <alex.yakunin@gmail.com>
2023-06-19 23:25:30 -0700

    wip: Rpc

--- src/Stl.Rpc/Infrastructure/RpcMessage.cs
+++ src/Stl.Rpc/Infrastructure/RpcMessage.cs
@@ -3,12 +3,12 @@ namespace Stl.Rpc.Infrastructure;
 [DataContract, MemoryPackable(GenerateType.VersionTolerant)]
 public partial class RpcMessage
 {
-    [DataMember(Order = 0), MemoryPackOrder(0)] public byte CallTypeId;
-    [DataMember(Order = 1), MemoryPackOrder(1)] public long CallId;
-    [DataMember(Order = 2), MemoryPackOrder(2)] public string Service;
-    [DataMember(Order = 3), MemoryPackOrder(3)] public string Method;
-    [DataMember(Order = 4), MemoryPackOrder(4)] public TextOrBytes ArgumentData;
-    [DataMember(Order = 5), MemoryPackOrder(5)] public List<RpcHeader>? Headers;
+    [JsonInclude, DataMember(Order = 0), MemoryPackOrder(0)] public byte CallTypeId;
+    [JsonInclude, DataMember(Order = 1), MemoryPackOrder(1)] public long CallId;
+    [JsonInclude, DataMember(Order = 2), MemoryPackOrder(2)] public string Service;
+    [JsonInclude, DataMember(Order = 3), MemoryPackOrder(3)] public string Method;
+    [JsonInclude, DataMember(Order = 4), MemoryPackOrder(4)] public TextOrBytes ArgumentData;
+    [JsonInclude, DataMember(Order = 5), MemoryPackOrder(5)] public List<RpcHeader>? Headers;
 
     [JsonConstructor, Newtonsoft.Json.JsonConstructor, MemoryPackConstructor]
     public RpcMessage(
