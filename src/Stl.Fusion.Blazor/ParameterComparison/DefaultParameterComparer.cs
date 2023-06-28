738d3c7751cd0ba9fd256b555151249e4d34a885
Alex Yakunin <alex.yakunin@gmail.com>
2023-06-28 04:30:06 -0700

    chore: make DefaultParameterComparer.cs behave exactly as Blazor behaves now

--- src/Stl.Fusion.Blazor/ParameterComparison/DefaultParameterComparer.cs
+++ src/Stl.Fusion.Blazor/ParameterComparison/DefaultParameterComparer.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.Components;
+
 namespace Stl.Fusion.Blazor;
 
-public sealed class DefaultParameterComparer : ParameterComparer
+public class DefaultParameterComparer : ParameterComparer
 {
-    public static DefaultParameterComparer Instance { get; } = new();
+    private static readonly Type EventCallbackInterfaceType = typeof(EventCallback)
+        .GetInterfaces()
+        .Single(x => Equals(x.Name, "IEventCallback"));
+
+    public static ParameterComparer Instance { get; } = new DefaultParameterComparer();
 
     // Mostly copied from Microsoft.AspNetCore.Components.ChangeDetection
     public override bool AreEqual(object? oldValue, object? newValue)
@@ -11,6 +17,7 @@ public sealed class DefaultParameterComparer : ParameterComparer
         var newIsNotNull = newValue != null;
         if (oldIsNotNull != newIsNotNull)
             return false; // One's null and the other isn't, so different
+
         if (oldIsNotNull) {
             // Both are not null (considering previous check)
             var oldValueType = oldValue!.GetType();
@@ -30,11 +37,13 @@ public sealed class DefaultParameterComparer : ParameterComparer
     // time. So we don't want a huge list of types to check (or would have to move to
     // a hashtable lookup, which is differently expensive). It's better not to include
     // uncommon types here even if they are known to be immutable.
+    // This logic assumes that no new System.TypeCode enum entries have been declared since 7.0, or at least that any new ones
+    // represent immutable types. If System.TypeCode changes, review this logic to ensure it is still correct.
+    // Supported immutable types : bool, byte, sbyte, short, ushort, int, uint, long, ulong, char, double,
+    //                             string, DateTime, decimal, Guid, Enum, EventCallBack, EventCallBack<>.
+    // For performance reasons, the following immutable types are not supported: IntPtr, UIntPtr, Type.
     private static bool IsKnownImmutableType(Type type)
-        => type.IsPrimitive
-            || type == typeof(string)
-            || type == typeof(DateTime)
-            || type == typeof(Type)
-            || type == typeof(decimal)
-            || type == typeof(Guid);
+        => Type.GetTypeCode(type) != TypeCode.Object
+            || type == typeof(Guid)
+            || EventCallbackInterfaceType.IsAssignableFrom(type);
 }
