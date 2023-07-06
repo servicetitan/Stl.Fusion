d2f1ec17ce860780eb360872b3e83b02a3bb6149
Alex Yakunin <alex.yakunin@gmail.com>
2023-07-05 17:31:14 -0700

    fix: RpcPeerStateMonitor may stop updating

--- src/Stl.Fusion/Extensions/RpcPeerStateMonitor.cs
+++ src/Stl.Fusion/Extensions/RpcPeerStateMonitor.cs
@@ -5,8 +5,10 @@ namespace Stl.Fusion.Extensions;
 public sealed class RpcPeerStateMonitor : WorkerBase
 {
     private readonly IMutableState<RpcPeerState?> _state;
+    private ILogger? _log;
 
     private IServiceProvider Services { get; }
+    private ILogger Log => _log ??= Services.LogFor(GetType());
 
     public RpcPeerRef PeerRef { get; set; } = RpcPeerRef.Default;
     public TimeSpan StartDelay { get; set; } = TimeSpan.FromSeconds(1);
@@ -20,40 +22,49 @@ public sealed class RpcPeerStateMonitor : WorkerBase
 
     protected override async Task OnRun(CancellationToken cancellationToken)
     {
-        await Task.Delay(StartDelay, cancellationToken).ConfigureAwait(false);
-        try {
+        while (true) {
+            await Task.Delay(StartDelay, cancellationToken).ConfigureAwait(false);
             var peer = Services.RpcHub().GetPeer(PeerRef);
             var clientPeer = peer as RpcClientPeer;
-            // This delay gives some time for peer to connect
-            using var cts = cancellationToken.LinkWith(peer.StopToken);
-            await foreach (var e in peer.ConnectionState.Events(cts.Token).ConfigureAwait(false)) {
-                var connectionState = e.Value;
-                var isConnected = connectionState.IsConnected();
-                var nextState = new RpcPeerState(isConnected, connectionState.Error);
+            Log.LogInformation("`{PeerRef}`: monitor started", PeerRef);
+            try {
+                // This delay gives some time for peer to connect
+                using var cts = cancellationToken.LinkWith(peer.StopToken);
+                await foreach (var e in peer.ConnectionState.Events(cts.Token).ConfigureAwait(false)) {
+                    var connectionState = e.Value;
+                    var isConnected = connectionState.IsConnected();
+                    var nextState = new RpcPeerState(isConnected, connectionState.Error);
 
-                _state.Value = nextState;
-                if (clientPeer != null && !isConnected) {
-                    // Client peer disconnected, we need to watch for ReconnectAt value change now
-                    for (var i = 0; i < 10; i++) {
-                        // ReSharper disable once MethodSupportsCancellation
-                        if (e.WhenNext().IsCompleted)
-                            break;
-                        if (clientPeer.ReconnectsAt is { } vReconnectsAt) {
-                            nextState = nextState with { ReconnectsAt = vReconnectsAt };
-                            _state.Value = nextState;
-                            break;
+                    _state.Value = nextState;
+                    if (clientPeer != null && !isConnected) {
+                        // Client peer disconnected, we need to watch for ReconnectAt value change now
+                        for (var i = 0; i < 10; i++) {
+                            // ReSharper disable once MethodSupportsCancellation
+                            if (e.WhenNext().IsCompleted)
+                                break;
+                            if (clientPeer.ReconnectsAt is { } vReconnectsAt) {
+                                nextState = nextState with { ReconnectsAt = vReconnectsAt };
+                                _state.Value = nextState;
+                                break;
+                            }
+
+                            await Task.Delay(50, cancellationToken).ConfigureAwait(false);
                         }
-
-                        await Task.Delay(50, cancellationToken).ConfigureAwait(false);
                     }
                 }
+                _state.Value = null;
             }
-            _state.Value = null;
-        }
-        catch (Exception) {
-            _state.Value = null;
-            if (cancellationToken.IsCancellationRequested)
-                throw;
+            catch (Exception e) {
+                _state.Value = null;
+                if (cancellationToken.IsCancellationRequested) {
+                    Log.LogInformation("`{PeerRef}`: monitor stopped", PeerRef);
+                    throw;
+                }
+                if (e is not OperationCanceledException)
+                    Log.LogError(e, "`{PeerRef}`: monitor failed, will restart", PeerRef);
+            }
+            if (peer.StopToken.IsCancellationRequested)
+                Log.LogWarning("`{PeerRef}`: peer is terminated, will restart", PeerRef);
         }
     }
 }
