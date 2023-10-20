c2580e5042345efa02eb6cb3e6716417cd026a77
Alex Yakunin <alex.yakunin@gmail.com>
2023-10-20 00:48:46 -0700

    feat: add TaskExt.CollectResults

--- src/Stl/Async/TaskExt.Collect.cs
+++ src/Stl/Async/TaskExt.Collect.cs
@@ -6,11 +6,16 @@ public static partial class TaskExt
 {
     // Collect - a bit more user-friendly version of Task.WhenAll
 
-    public static Task<T[]> Collect<T>(this IEnumerable<Task<T>> tasks, int concurrency = 0) 
+    public static Task<T[]> Collect<T>(this IEnumerable<Task<T>> tasks, int concurrency = 0)
         => concurrency <= 0 ? Task.WhenAll(tasks) : CollectConcurrently(tasks, concurrency);
 
     public static Task Collect(this IEnumerable<Task> tasks, int concurrency = 0)
         => concurrency <= 0 ? Task.WhenAll(tasks) : CollectConcurrently(tasks, concurrency);
+
+    public static Task<Result<T>[]> CollectResults<T>(this IEnumerable<Task<T>> tasks, int concurrency = 0)
+        => concurrency <= 0 ? ToResults(tasks.ToList()) : CollectResultsConcurrently(tasks, concurrency);
+
+    // Private methods
 
     private static async Task<T[]> CollectConcurrently<T>(this IEnumerable<Task<T>> tasks, int concurrency)
     {
@@ -19,17 +24,10 @@ public static partial class TaskExt
         var i = 0;
         foreach (var task in tasks) {
             list.Add(task);
-            if (concurrency > runningCount) {
+            if (concurrency > runningCount)
                 runningCount++;
-            }
-            else {
-                try {
-                    await list[i++].ConfigureAwait(false);
-                }
-                catch {
-                    // Intended
-                }
-            }
+            else
+                await list[i++].SilentAwait(false);
         }
         return await Task.WhenAll(list).ConfigureAwait(false);
     }
@@ -41,18 +39,35 @@ public static partial class TaskExt
         var i = 0;
         foreach (var task in tasks) {
             list.Add(task);
-            if (concurrency > runningCount) {
+            if (concurrency > runningCount)
                 runningCount++;
-            }
-            else {
-                try {
-                    await list[i++].ConfigureAwait(false);
-                }
-                catch {
-                    // Intended
-                }
-            }
+            else
+                await list[i++].SilentAwait(false);
         }
         await Task.WhenAll(list).ConfigureAwait(false);
+    }
+
+    private static async Task<Result<T>[]> CollectResultsConcurrently<T>(this IEnumerable<Task<T>> tasks, int concurrency)
+    {
+        var list = new List<Task<T>>();
+        var runningCount = 0;
+        var i = 0;
+        foreach (var task in tasks) {
+            list.Add(task);
+            if (concurrency > runningCount)
+                runningCount++;
+            else
+                await list[i++].SilentAwait(false);
+        }
+        return await ToResults(list).ConfigureAwait(false);
+    }
+
+    private static async Task<Result<T>[]> ToResults<T>(List<Task<T>> list)
+    {
+        await Task.WhenAll(list).SilentAwait(false);
+        var result = new Result<T>[list.Count];
+        for (var i = 0; i < result.Length; i++)
+            result[i] = list[i].ToResultSynchronously();
+        return result;
     }
 }
