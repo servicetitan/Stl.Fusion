ca101a5e77bc69742f69438e3b71639ef3c34c00
Alex Yakunin <alex.yakunin@gmail.com>
2023-12-05 08:28:35 -0800

    fix: TerminalErrorDetector should return true on JSDisconnectedException

--- src/Stl/TerminalError.cs
+++ src/Stl/TerminalError.cs
@@ -11,6 +11,9 @@ public static class TerminalError
     {
         if (error is not ObjectDisposedException ode)
             return false;
+        if (Equals(error.GetType().Name, "JSDisconnectedException"))
+            return true; // This is specific to Blazor Server, it also indicates the scope is going to die soon
+
 #if NETSTANDARD2_0
         return ode.ObjectName.Contains("IServiceProvider")
             || ode.Message.Contains("'IServiceProvider'");
